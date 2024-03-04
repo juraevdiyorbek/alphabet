4bbdd28cd9310e2536195b8bc1443df16814086e
Diyorbek Juraev <85333075+juraevdiyorbek@users.noreply.github.com>
2024-03-04 15:02:59 +0500

    Update alifbo.py
    comments added

--- alifbo.py
+++ alifbo.py
@@ -1,13 +1,14 @@
 def get_letters(text):
+    # - bunga bolib yuboramiz
     text = text.split("-")
     if not text[0] < text[1]:
         return "Alifbor tartibida kiriting"
-
+    # shartlar tekshiriladi yani ikkalasiyam kattami yoki kichik
     if 'a' <= text[0] <= 'z'and not 'a' <= text[1] <= 'z':
             return "Iltimos ikkalasini kichik harfda yoki katta harfda kiriting"
     elif 'A' <= text[0] <= 'Z'and not 'A' <= text[1] <= 'Z':
             return "Iltimos ikkalasini kichik harfda yoki katta harfda kiriting"
-        
+        #Agar shartlar bajarilsa harflarni o'zuga qoshib boradi string 
     string = str()
     for i in range(ord(text[0]),ord(text[1])+1):
         string+=chr(i)
